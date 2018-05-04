d97e57859cdd84dcca8f0e78d7c04d6fd4c5d165
dyh <1015462828@qq.com>
2018-05-04 20:54:07 +0800

    修改了在子线程无法更新ui的错误

--- app/src/main/java/com/example/kingdom/myapplication_1/MainActivity.java
+++ app/src/main/java/com/example/kingdom/myapplication_1/MainActivity.java
@@ -1,12 +1,20 @@
 package com.example.kingdom.myapplication_1;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/com/example/kingdom/myapplication_1/four_Activity.java
+++ app/src/main/java/com/example/kingdom/myapplication_1/four_Activity.java
@@ -1,9 +1,9 @@
 package com.example.kingdom.myapplication_1;
 
-import android.graphics.BitmapFactory;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -12,12 +12,59 @@ import java.util.Timer;
 import java.util.TimerTask;
 
 public class four_Activity extends AppCompatActivity {
+    final static int UPDATE_IMAGEVIEW = 1;
+    Timer timer = new Timer();
+    int cnt = 0;
+    private Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case UPDATE_IMAGEVIEW:
+                    if (cnt % 5 == 0) {
+                        ImageView imageView = (ImageView) findViewById(R.id.image_1);
+                        imageView.setImageLevel(0);
+                    }
+                    if (cnt % 5 == 1) {
+                        ImageView imageView = (ImageView) findViewById(R.id.image_1);
+                        imageView.setImageLevel(1);
+                    }
+                    if (cnt % 5 == 2) {
+                        ImageView imageView = (ImageView) findViewById(R.id.image_1);
+                        imageView.setImageLevel(2);
+                    }
+                    if (cnt % 5 == 3) {
+                        ImageView imageView = (ImageView) findViewById(R.id.image_1);
+                        imageView.setImageLevel(3);
+                    }
+                    if (cnt % 5 == 4) {
+                        ImageView imageView = (ImageView) findViewById(R.id.image_1);
+                        imageView.setImageLevel(4);
+                    }
+                    cnt++;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+    };
+    TimerTask task = new TimerTask() {
+        @Override
+        public void run() {
+            Message message = new Message();
+            message.what = UPDATE_IMAGEVIEW;
+            handler.sendMessage(message);
+
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_four_);
-        Button button_1=(Button)findViewById(R.id.button_1);
+        Button button_1 = (Button) findViewById(R.id.button_1);
         button_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -29,46 +76,23 @@ public class four_Activity extends AppCompatActivity {
         this.setContentView(imageView);*/
         //ImageView imageView=(ImageView)findViewById(R.id.image_1);
         //imageView.setImageLevel(1);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                timer.schedule(task, 0, 1000);
+            }
+        }).start();
 
     }
-    int cnt=0;
-    Timer timer=new Timer();
-    TimerTask task=new TimerTask() {
-        @Override
-        public void run() {
-            if(cnt%5==0){
-                ImageView imageView=(ImageView)findViewById(R.id.image_1);
-                imageView.setImageLevel(0);
-                cnt=0;
-            }
-            if(cnt%5==1){
-                ImageView imageView=(ImageView)findViewById(R.id.image_1);
-                imageView.setImageLevel(1);
-            }
-            if(cnt%5==2){
-                ImageView imageView=(ImageView)findViewById(R.id.image_1);
-                imageView.setImageLevel(2);
-            }
-            if(cnt%5==3){
-                ImageView imageView=(ImageView)findViewById(R.id.image_1);
-                imageView.setImageLevel(3);
-            }
-            if(cnt%5==4){
-                ImageView imageView=(ImageView)findViewById(R.id.image_1);
-                imageView.setImageLevel(4);
-            }
-            cnt++;
-            //Log.d("four_Activity","lllllllllllllllllllllllllllllll");
-        }
-    };
+
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
-        timer.schedule(task,0,1000);
     }
+
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         timer.cancel();
     }
